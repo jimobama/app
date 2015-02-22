68f3bfae5fe2b615c08796ee997ea8fc5037afd8
jimobama <isrealism2003@yahoo.com>
2015-02-22 04:12:10 +0000

    Framework done

---
+++ entities/Object.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TObjects
+ *
+ * @author jimobama
+ * what to make such that all my objects have the following methods
+ */
+abstract class Object {
+   
+  abstract function __construct();  
+   function __destruct()
+    {
+        
+    }
+  function toString()
+    {
+        return "";
+    }
+ protected function setError($err)
+  {   if(trim($err)=="")
+      return null;
+      $this->error=$err;
+  }
+ public function getError()
+  {
+      return $this->error;
+  }
+ 
+  abstract  function validated();
+  
+}
+
+?>
